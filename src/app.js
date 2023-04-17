cc70b82c6414cf7e89dc39cb9e8a810b6ddab7eb
LordAkirast <darkwarish@gmail.com>
2023-04-17 17:14:22 -0300

    Now server checks for conflicts and add an item correctly.

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import { MongoClient } from 'mongodb';
 import dotenv from "dotenv";
+import Joi from 'joi';
 
-
+dotenv.config();
 const app = express();
 app.use(cors());
-dotenv.config();
- 
+app.use(express.json())
+
+
 // Todas as rotas que forem criadas daqui para baixo poderão ser
 // acessadas através de um front-end
 
@@ -16,9 +18,15 @@ dotenv.config();
 let db
 const Mongoclient = new MongoClient(process.env.DATABASE_URL)
 Mongoclient.connect()
-.then(() => db = Mongoclient.db())
-.catch((err) => console.log(err.message))
+    .then(() => db = Mongoclient.db())
+    .catch((err) => console.log(err.message))
 
+
+const pessoas = [];
+
+const dataSchema = Joi.object({
+    name: Joi.string().required()
+})
 
 app.get("/pessoa", (req, res) => {
     const pessoa = JSON.stringify(pessoas);
@@ -26,18 +34,37 @@ app.get("/pessoa", (req, res) => {
 })
 
 app.get("/lista-pessoas", (req, res) => {
-    const pessoas = [{nome: "João", idade: 30},{nome: "Maria", idade: 20}];
+    const pessoas = [{ nome: "João", idade: 30 }, { nome: "Maria", idade: 20 }];
     res.send(pessoas);
 })
 
-const pessoas = [];
 
-app.post("/pessoa", (req, res) => {
-    const pessoa = req.body;
-    pessoas.push(pessoa);
+
+app.post("/participants", async (req, res) => {
+    const { name } = req.body;
+    const { error } = dataSchema.validate({ name })
+
+
+    if (error) {
+        const errorMessage = error.details.map((err) => err.message);
+        return res.status(422).json({ error: errorMessage });
+    }
+
+
+    try {
+        const userExists = await db.collection("dbUol").findOne({ name })
+
+        if (userExists) {
+            return res.sendStatus(409)
+        }
+        await db.collection("dbUol").insertOne({ name })
+    } catch (error) {
+
+    }
+    pessoas.push(name);
     res.send(pessoas);
-  });
-  
+});
+
 
 
 app.listen(5000, () => console.log("Running server on port 5000"));
